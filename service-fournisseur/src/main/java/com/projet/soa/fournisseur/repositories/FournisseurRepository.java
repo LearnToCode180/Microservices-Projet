1681155f44007697c33f3a87d922203d0c285c37
Azdad Anas <65866281+LearnToCode180@users.noreply.github.com>
2021-08-12 16:26:34 +0100

    Delete service-fournisseur directory

--- service-fournisseur/src/main/java/com/projet/soa/fournisseur/repositories/FournisseurRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projet.soa.fournisseur.repositories;
-
-import com.projet.soa.fournisseur.models.Fournisseur;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FournisseurRepository extends MongoRepository<Fournisseur, Integer> {
-}
