33f2710b612e4940cd26531a7a2ee19ad36fc0f6
Azdad Anas <65866281+LearnToCode180@users.noreply.github.com>
2021-08-12 16:26:09 +0100

    Delete service-affectations/service-affectations directory

--- service-affectations/service-affectations/src/main/java/com/ressources/materielles/serviceaffectations/repositories/AffectationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ressources.materielles.serviceaffectations.repositories;
-
-import com.ressources.materielles.serviceaffectations.models.Affectation;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AffectationRepository extends MongoRepository<Affectation, Integer> {
-}
