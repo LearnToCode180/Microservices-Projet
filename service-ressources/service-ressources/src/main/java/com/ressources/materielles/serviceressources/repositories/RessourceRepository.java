a6a8ad2a27f3cf68744de08acd45a1d001c95c78
Azdad Anas <65866281+LearnToCode180@users.noreply.github.com>
2021-08-12 16:26:47 +0100

    Delete service-ressources/service-ressources directory

--- service-ressources/service-ressources/src/main/java/com/ressources/materielles/serviceressources/repositories/RessourceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ressources.materielles.serviceressources.repositories;
-
-import com.ressources.materielles.serviceressources.models.Ressource;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RessourceRepository extends MongoRepository<Ressource, Integer> {
-}
